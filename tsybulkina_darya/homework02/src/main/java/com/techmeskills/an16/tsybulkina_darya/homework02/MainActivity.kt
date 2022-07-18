63c9659a09d7cd9eebfcc5f363a5dbb3a6e0e571
DashaTsybulkina <cybulkinadara@gmail.com>
2022-07-18 21:10:55 +0300

    Create Homework2

--- tsybulkina_darya/homework02/src/main/java/com/techmeskills/an16/tsybulkina_darya/homework02/MainActivity.kt
+++ tsybulkina_darya/homework02/src/main/java/com/techmeskills/an16/tsybulkina_darya/homework02/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.techmeskills.an16.tsybulkina_darya.homework01
+package com.techmeskills.an16.tsybulkina_darya.homework02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.container, FirstFragment.newInstance())
+        fragmentTransaction.commit()
+
     }
 }
