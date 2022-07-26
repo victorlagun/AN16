921ad8b874a8edf090a862c2df87ac4d180bb4bf
AlexanderElissejew <AlexanderElissejew@yandex.by>
2022-07-26 22:00:27 +0300

    Eliseyev03

---
+++ eliseyev_aleksandr_homework03/src/main/java/com/example/eliseyev_aleksandr_homework03/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eliseyev_aleksandr_homework03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
