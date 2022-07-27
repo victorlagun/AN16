96660face1ba36ee28d7c4e3f345f79c1fe08850
AlexanderElissejew <AlexanderElissejew@yandex.by>
2022-07-27 23:10:17 +0300

    Eliseyev03

--- eliseyev_aleksandr_homework03/src/main/java/com/example/eliseyev_aleksandr_homework03/ui/game/GameViewModel.kt
+++ eliseyev_aleksandr_homework03/src/main/java/com/example/eliseyev_aleksandr_homework03/ui/game/GameViewModel.kt
@@ -16,9 +16,11 @@ class GameViewModel : ViewModel() {
     private var score = 0
     private var currentWordCount = 0
 
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+
 
     private var _count = 0
 
@@ -28,6 +30,37 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 
 }
