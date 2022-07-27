dffac45cd515463567ea38125e34e47e748f6aa7
AlexanderElissejew <AlexanderElissejew@yandex.by>
2022-07-27 22:38:08 +0300

    Eliseyev03

---
+++ eliseyev_aleksandr_homework03/src/main/java/com/example/eliseyev_aleksandr_homework03/ui/game/GameViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.eliseyev_aleksandr_homework03.ui.game
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel containing the app data and methods to process the data
+ */
+class GameViewModel : ViewModel() {
+
+
+}
