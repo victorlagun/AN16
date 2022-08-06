a1aacbd1237c8d7676dfa3d3d2dfee6b03238747
DashaTsybulkina <cybulkinadara@gmail.com>
2022-08-06 13:24:06 +0300

    display an internet image

---
+++ tsybulkina_darya/homework05/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.launch
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData String that stores the most recent response
+    private val _response = MutableLiveData<String>()
+
+    // The external immutable LiveData for the response String
+    val response: LiveData<String>
+        get() = _response
+
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
+
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    private fun getMarsRealEstateProperties() {
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties()
+                _response.value =
+                    "Success: ${listResult.size} Mars properties retrieved"
+                if (listResult.size>0){
+                    _property.value = listResult[0]
+                }
+            }catch (e:Exception){
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
+}
