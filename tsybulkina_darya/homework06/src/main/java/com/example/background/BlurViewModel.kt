3195a595478f423adf1ebad0dae18b7bf4880043
DashaTsybulkina <cybulkinadara@gmail.com>
2022-08-07 13:48:43 +0300

    unique work

--- tsybulkina_darya/homework06/src/main/java/com/example/background/BlurViewModel.kt
+++ tsybulkina_darya/homework06/src/main/java/com/example/background/BlurViewModel.kt
@@ -22,10 +22,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.background.workers.BlurWorker
 import com.example.background.workers.CleanupWorker
 import com.example.background.workers.SaveImageToFileWorker
@@ -46,10 +43,12 @@ class BlurViewModel(application: Application) : ViewModel() {
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
-        var continuation = worker.beginWith(
-            OneTimeWorkRequest
-                .from(CleanupWorker::class.java)
-        )
+        var continuation = worker
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
         for (i in 0 until blurLevel) {
             val workRequest =
                 OneTimeWorkRequestBuilder<BlurWorker>()
