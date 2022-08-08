e441e4d5bd6dac12d8ad0820338d31b6aef801c0
MiseikoDenis <den.miseiko@mail.ru>
2022-08-08 17:13:12 +0300

    4. Task: Display a grid of images with a RecyclerView

--- miseyko_denis/homework05/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ miseyko_denis/homework05/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -19,8 +19,12 @@ package com.example.android.marsrealestate
 
 import android.widget.ImageView
 import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
@@ -35,4 +39,10 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             )
             .into(imgView)
     }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>){
+val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
 }
