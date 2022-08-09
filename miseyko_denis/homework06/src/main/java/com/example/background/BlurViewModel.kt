363e017c054a1349df2edcacfe710f5889e7d74a
MiseikoDenis <den.miseiko@mail.ru>
2022-08-09 16:01:21 +0300

    11. Work constraints

--- miseyko_denis/homework06/src/main/java/com/example/background/BlurViewModel.kt
+++ miseyko_denis/homework06/src/main/java/com/example/background/BlurViewModel.kt
@@ -71,8 +71,14 @@ class BlurViewModel(application: Application) : ViewModel() {
             continuation = continuation.then(blurBuilder.build())
         }
 
+        // Create charging constraint
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .build()
+
         // Add WorkRequest to save the image to the filesystem
         val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .setConstraints(constraints)
             .addTag(TAG_OUTPUT)
             .build()
 
