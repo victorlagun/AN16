e001ff47293345f49c86c70214011bffac112d39
AlexanderElissejew <AlexanderElissejew@yandex.by>
2022-08-10 21:49:55 +0300

    Eliseyev05. Part 1 Task 5

--- eliseyev_aleksandr-homework05/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ eliseyev_aleksandr-homework05/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -19,9 +19,8 @@ package com.example.android.marsrealestate.network
 
 import com.squareup.moshi.Json
 
-class MarsProperty(
-    val id: String,
-    @Json(name = "img_src") val img_src: String,
+data class MarsProperty(
+    val id: String, @Json(name = "img_src") val imgSrcUrl: String,
     val type: String,
     val price: Double
 )
