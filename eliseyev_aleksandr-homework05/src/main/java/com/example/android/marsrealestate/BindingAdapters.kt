7b6c8d56ce357834de8c8c486b7f044181bf613b
AlexanderElissejew <AlexanderElissejew@yandex.by>
2022-08-11 23:12:52 +0300

    Eliseyev05. Part 2 Task 5 Final

--- eliseyev_aleksandr-homework05/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ eliseyev_aleksandr-homework05/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -58,9 +58,11 @@ fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
         MarsApiStatus.ERROR -> {
             statusImageView.visibility = View.VISIBLE
             statusImageView.setImageResource(R.drawable.ic_connection_error)
-        }
-        MarsApiStatus.DONE -> {
+        } else -> {
+
             statusImageView.visibility = View.GONE
+
         }
+
     }
 }
