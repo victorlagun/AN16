8e68112349f971723b133fa3bc6e345a372fbd65
DashaTsybulkina <cybulkinadara@gmail.com>
2022-08-14 17:05:00 +0300

    create binding adapters

--- tsybulkina_darya/homework07/src/main/java/com/techmeskills/an16/homework04/adapter/ListItemSleepViewHolder.kt
+++ tsybulkina_darya/homework07/src/main/java/com/techmeskills/an16/homework04/adapter/ListItemSleepViewHolder.kt
@@ -12,21 +12,8 @@ import com.techmeskills.an16.lagun_victor.homework04.databinding.ListItemSleepNi
 class ListItemSleepViewHolder private constructor(val binding: ListItemSleepNightBinding):RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: SleepNight) {
-        val res = itemView.context.resources
-        binding.sleepLength.text =
-            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-        binding.qualityImage.setImageResource(
-            when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_launcher_sleep_tracker_foreground
-            }
-        )
+        binding.sleep = item
+        binding.executePendingBindings()
     }
 
     companion object {
---
+++ tsybulkina_darya/homework07/src/main/java/com/techmeskills/an16/homework04/sleeptracker/BindingUtils.kt
@@ -0,0 +1,38 @@
+package com.techmeskills.an16.homework04.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.techmeskills.an16.homework04.convertDurationToFormatted
+import com.techmeskills.an16.homework04.convertNumericQualityToString
+import com.techmeskills.an16.homework04.database.SleepNight
+import com.techmeskills.an16.lagun_victor.homework04.R
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+}
+
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+}
+
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(
+        when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+
+            3 -> R.drawable.ic_sleep_3
+
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_launcher_sleep_tracker_foreground
+        }
+    )
+}
