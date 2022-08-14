bb6872d62dad1dbaf445f2b7d71b821eb8960823
DashaTsybulkina <cybulkinadara@gmail.com>
2022-08-14 15:54:22 +0300

    use DataBinding with RecyclerView

--- tsybulkina_darya/homework07/src/main/java/com/techmeskills/an16/homework04/adapter/ListItemSleepViewHolder.kt
+++ tsybulkina_darya/homework07/src/main/java/com/techmeskills/an16/homework04/adapter/ListItemSleepViewHolder.kt
@@ -1,27 +1,22 @@
 package com.techmeskills.an16.homework04.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.techmeskills.an16.homework04.convertDurationToFormatted
 import com.techmeskills.an16.homework04.convertNumericQualityToString
 import com.techmeskills.an16.homework04.database.SleepNight
 import com.techmeskills.an16.lagun_victor.homework04.R
+import com.techmeskills.an16.lagun_victor.homework04.databinding.ListItemSleepNightBinding
 
-class ListItemSleepViewHolder private constructor(itemView:View):RecyclerView.ViewHolder(itemView) {
-    val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-    val quality: TextView = itemView.findViewById(R.id.quality_string)
-    val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+class ListItemSleepViewHolder private constructor(val binding: ListItemSleepNightBinding):RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: SleepNight) {
         val res = itemView.context.resources
-        sleepLength.text =
+        binding.sleepLength.text =
             convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        quality.text = convertNumericQualityToString(item.sleepQuality, res)
-        qualityImage.setImageResource(
+        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
+        binding.qualityImage.setImageResource(
             when (item.sleepQuality) {
                 0 -> R.drawable.ic_sleep_0
                 1 -> R.drawable.ic_sleep_1
@@ -37,8 +32,9 @@ class ListItemSleepViewHolder private constructor(itemView:View):RecyclerView.Vi
     companion object {
         fun from(parent: ViewGroup): ListItemSleepViewHolder {
             val layoutInflater = LayoutInflater.from(parent.context)
-            val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
-            return ListItemSleepViewHolder(view)
+            val binding =
+                ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+            return ListItemSleepViewHolder(binding)
         }
     }
 }
