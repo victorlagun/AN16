f97e208b7b81babc8001e3d1352d908ed7c99bf5
anton <antonlazovski111@gmail.com>
2022-08-24 22:42:37 +0300

    First Commit Getting data from the internet

--- lazovskiy_anton/homework06/src/main/java/com/techmeskills/an16/lazovski_anton/homework06/MainActivity.kt
+++ lazovskiy_anton/homework06/src/main/java/com/techmeskills/an16/lazovski_anton/homework06/MainActivity.kt
@@ -1,9 +1,33 @@
-package com.techmeskills.an16.template.homework
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-import androidx.appcompat.app.AppCompatActivity
+package com.techmeskills.an16.lazovski_anton.homework06
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import com.techmeskills.an16.lazovski_anton.homework06.R
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Our MainActivity is only responsible for setting the content view that contains the
+     * Navigation Host.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
