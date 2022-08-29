51eee36d6522517932c692da30caccd232ff3635
anton <antonlazovski111@gmail.com>
2022-08-29 22:34:52 +0300

    First commit

--- lazovskiy_anton/homework08/src/main/java/com/techmeskills/an16/lazovski_anton/homework08/MainActivity.kt
+++ lazovskiy_anton/homework08/src/main/java/com/techmeskills/an16/lazovski_anton/homework08/MainActivity.kt
@@ -1,11 +1,43 @@
-package com.techmeskills.an16.template.homework
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import androidx.appcompat.app.AppCompatActivity
+package com.example.android.navigation
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.techmeskills.lazovski_anton.homework08.R
+import com.techmeskills.lazovski_anton.homework08.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHost = findNavController(R.id.myNavHostFragment)
     }
+
+    // TODO (01) Create the new TitleFragment
+    // Select File->New->Fragment->Fragment (Blank)
+
+    // TODO (02) Clean up the new TitleFragment
+    // In our new TitleFragment
+
+    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
+    // In our new TitleFragment
+    // R.layout.fragment_title
 }
